0bd4ef05f08d940afcd146a62177cef14a6b1361
ndrean <nevendrean@yahoo.fr>
2026-07-29 08:51:52 +0200

    anubis MCP server & Oauth Boruta

--- lib/hex_gh_web/router.ex
+++ lib/hex_gh_web/router.ex
@@ -17,6 +17,7 @@ defmodule HexGhWeb.Router do
   pipeline :oauth_browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug HexGhWeb.Plugs.SessionAuth
